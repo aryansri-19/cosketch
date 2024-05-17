36ecf54b0c68fc569ee1bbf5dee1fc6fe265da72
aryansri-19 <aryansri.19@gmail.com>
2024-05-17 12:09:46 +0530

    Removed auth and models

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@
 const Page = () => {
   return ( 
     <div>
+      <canvas id="canvas" width="800" height="600">
+      </canvas>
     </div>
    );
 }
--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,22 +1,14 @@
-import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import UserProfile from "./UserProfile";
 
 const Navbar = async () => {
-    const user = await auth();
     return ( 
         <>
             <nav className="bg-background dark:bg-foregorund flex justify-between items-center m-auto pr-5 pl-8 max-h-12 sticky">
-                <Image src='/icon.png' width={50} height={50} alt='CoSketch Logo'/>
-                <Link href='/'><h1 className="text-3xl font-bold text-orange-500 pl-20">CoSketch</h1></Link>
-                <section className="flex justify-between space-x-5">
-                    <Link href='/invite'><Button variant='custom' size='sm'>Invite</Button></Link>
-                    { (user?.user && user.user?.image) ? 
-                    <UserProfile userData={user?.user}/>
-                    : <Link href='/sign-in'><Button variant='custom' size='sm'>Sign In</Button></Link>}
-                </section>
+                <Link href='/'><Image src='/icon.png' width={50} height={50} alt='CoSketch Logo'/></Link>
+                <h1 className="text-3xl font-bold text-orange-500 pl-20">CoSketch</h1>
+                <Link href='/invite'><Button variant='custom' size='default' className="text-foreground">Invite</Button></Link>
             </nav>
         </>
     );
