cf672c637a931876c6f5adf122ff283d8f527612
aryansri-19 <aryansri.19@gmail.com>
2024-05-19 09:09:42 +0530

    Drawing init

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 const Navbar = async () => {
     return ( 
         <>
-            <nav className="bg-background dark:bg-foregorund flex justify-between items-center m-auto pr-5 pl-8 max-h-12 sticky">
+            <nav className="bg-background dark:bg-foregorund flex justify-between items-center m-auto max-h-12 sticky top-0 left-0 right-0">
+                <div className="order-[-1] pl-8">
                 <Link href='/'><Image src='/icon.png' width={50} height={50} alt='CoSketch Logo'/></Link>
-                <h1 className="text-3xl font-bold text-orange-500 pl-20">CoSketch</h1>
-                <Link href='/invite'><Button variant='custom' size='default' className="text-foreground">Invite</Button></Link>
+                </div>
+                <h1 className="text-3xl font-bold text-orange-500">CoSketch</h1>
+                <Link href='/invite' className="pr-5"><Button variant='custom' size='default' className="text-foreground">Invite</Button></Link>
             </nav>
         </>
     );
